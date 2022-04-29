3b24ef8fd32556ef1e025551dbb107c44a0ad2ae
Nicolas Forstner <nls.forstner@gmail.com>
2022-04-29 21:06:46 +0100

    added viking script

---
+++ viking/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=nls.forstner@gmail.com
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=10gb
+#SBATCH --output=./viking/job-%j.log
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+
+echo "Loading CUDA 11.0"
+module load system/CUDA/11.0.2-GCC-9.3.0
+
+pushd /users/nf711/scratch/zgan/
+. .venv/bin/activate
+make py-deps
+./cli.py train
+popd
